3057b81cce51ff07c131169abec68ef9ddeb4d67
Alex Rocha <alex.rochas@yahoo.com.br>
2015-10-15 00:14:10 -0300

    Boilerplate for unit tests.

---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+import vimmock
+vimmock.patch_vim()
---
+++ tests/test_phpunit.py
@@ -0,0 +1,11 @@
+import vim
+import unittest
+import plugin.phpunit as phpunit
+
+class  PhpUnitTest(unittest.TestCase):
+
+    def test_holder(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
